dcebcec9c0cf1aa41705bf9df900ac067ae8e569
stackprobe <stackprobe@gmail.com>
2016-04-28 00:23:18 +0900

    a

--- charlotte/tools/ReflecTools.java
+++ charlotte/tools/ReflecTools.java
@@ -32,13 +32,15 @@ public class ReflecTools {
 
 	public static Method getDeclaredMethod(Class<?> classObj, String methodName, Class<?>[] paramTypes) {
 		for(; ; ) {
-			for(Method mo : classObj.getDeclaredMethods()) {
-				if(methodName.equals(mo.getName())) {
-					Class<?>[] moParamTypes = mo.getParameterTypes();
+			for(int fuzzyLv = 0; fuzzyLv <= 1; fuzzyLv++) {
+				for(Method mo : classObj.getDeclaredMethods()) {
+					if(methodName.equals(mo.getName())) {
+						Class<?>[] moParamTypes = mo.getParameterTypes();
 
-					if(isSameTypeArray(paramTypes, moParamTypes)) {
-						mo.setAccessible(true);
-						return mo;
+						if(isSameTypeArray(paramTypes, moParamTypes, fuzzyLv)) {
+							mo.setAccessible(true);
+							return mo;
+						}
 					}
 				}
 			}
@@ -61,12 +63,14 @@ public class ReflecTools {
 	}
 
 	public static Constructor<?> getDeclaredCtor(Class<?> classObj, Class<?>[] paramTypes) {
-		for(Constructor<?> ctor : classObj.getDeclaredConstructors()) {
-			Class<?>[] ctorParamTypes = ctor.getParameterTypes();
+		for(int fuzzyLv = 0; fuzzyLv <= 1; fuzzyLv++) {
+			for(Constructor<?> ctor : classObj.getDeclaredConstructors()) {
+				Class<?>[] ctorParamTypes = ctor.getParameterTypes();
 
-			if(isSameTypeArray(paramTypes, ctorParamTypes)) {
-				ctor.setAccessible(true);
-				return ctor;
+				if(isSameTypeArray(paramTypes, ctorParamTypes, fuzzyLv)) {
+					ctor.setAccessible(true);
+					return ctor;
+				}
 			}
 		}
 		return null;
@@ -81,71 +85,54 @@ public class ReflecTools {
 		return types;
 	}
 
-	private static boolean isSameTypeArray(Class<?>[] t1, Class<?>[] t2) {
+	private static boolean isSameTypeArray(Class<?>[] t1, Class<?>[] t2, int fuzzyLv) {
 		if(t1.length != t2.length) {
 			return false;
 		}
 		for(int index = 0; index < t1.length; index++) {
-			if(isSameType(t1[index], t2[index]) == false) {
+			if(isSameType(t1[index], t2[index], fuzzyLv) == false) {
 				return false;
 			}
 		}
+		//System.out.println("fuzzyLv: " + fuzzyLv); // test
 		return true;
 	}
 
-	private static boolean isSameType(Class<?> t1, Class<?> t2) {
-		return univPrimitive(t1.getName()).equals(univPrimitive(t2.getName()));
-	}
+	private static boolean isSameType(Class<?> t1, Class<?> t2, int fuzzyLv) {
+		String n1 = t1.getName();
+		String n2 = t2.getName();
 
-	// XXX m(int a), m(Integer a) みたいなメソッド、コンストラクタが混在しているとアウト
+		if(1 <= fuzzyLv) {
+			n1 = univPrimitive(n1);
+			n2 = univPrimitive(n2);
+		}
+		return n1.equals(n2);
+	}
 
 	private static String univPrimitive(String name) {
-		//System.out.println("name.1: " + name); // test
-		name = univPrimitive_main(name);
-		//System.out.println("name.2: " + name); // test
-		return name;
-	}
-
-	private static String univPrimitive_main(String name) {
-		for(String[] pair : _prmtvPairs) {
+		for(String[] pair : _primitivePairs) {
 			if(name.equals(pair[0])) {
 				return pair[1];
 			}
 		}
-		for(String[] pair : _prmtvArrPairs) {
-			if(name.endsWith(pair[0])) {
-				return name.substring(0, name.length() - pair[0].length()) + pair[1];
-			}
-		}
 		return name;
 	}
 
-	private static List<String[]> _prmtvPairs;
-	private static List<String[]> _prmtvArrPairs;
+	private static List<String[]> _primitivePairs;
 
 	static {
-		_prmtvPairs = new ArrayList<String[]>();
+		_primitivePairs = new ArrayList<String[]>();
 
-		addPrmtvPair(_prmtvPairs, Boolean.class, boolean.class);
-		addPrmtvPair(_prmtvPairs, Byte.class, byte.class);
-		addPrmtvPair(_prmtvPairs, Character.class, char.class);
-		addPrmtvPair(_prmtvPairs, Integer.class, int.class);
-		addPrmtvPair(_prmtvPairs, Long.class, long.class);
-		addPrmtvPair(_prmtvPairs, Float.class, float.class);
-		addPrmtvPair(_prmtvPairs, Double.class, double.class);
-
-		_prmtvArrPairs = new ArrayList<String[]>();
-
-		addPrmtvPair(_prmtvArrPairs, new Boolean[0].getClass(), new boolean[0].getClass());
-		addPrmtvPair(_prmtvArrPairs, new Byte[0].getClass(), new byte[0].getClass());
-		addPrmtvPair(_prmtvArrPairs, new Character[0].getClass(), new char[0].getClass());
-		addPrmtvPair(_prmtvArrPairs, new Integer[0].getClass(), new int[0].getClass());
-		addPrmtvPair(_prmtvArrPairs, new Long[0].getClass(), new long[0].getClass());
-		addPrmtvPair(_prmtvArrPairs, new Float[0].getClass(), new float[0].getClass());
-		addPrmtvPair(_prmtvArrPairs, new Double[0].getClass(), new double[0].getClass());
+		addPrimitivePair(_primitivePairs, Boolean.class, boolean.class);
+		addPrimitivePair(_primitivePairs, Byte.class, byte.class);
+		addPrimitivePair(_primitivePairs, Character.class, char.class);
+		addPrimitivePair(_primitivePairs, Integer.class, int.class);
+		addPrimitivePair(_primitivePairs, Long.class, long.class);
+		addPrimitivePair(_primitivePairs, Float.class, float.class);
+		addPrimitivePair(_primitivePairs, Double.class, double.class);
 	}
 
-	private static void addPrmtvPair(List<String[]> dest, Class<?> t1, Class<?> t2) {
+	private static void addPrimitivePair(List<String[]> dest, Class<?> t1, Class<?> t2) {
 		dest.add(new String[] { t1.getName(), t2.getName() });
 	}
 
