3cd8115846b7043ae876f3800177f80cf283547f
stackprobe <stackprobe@gmail.com>
2016-06-25 02:15:44 +0900

    a

--- charlotte_test/tools/EraDateTest.java
+++ charlotte_test/tools/EraDateTest.java
@@ -1,17 +1,19 @@
 package charlotte_test.tools;
 
 import charlotte.tools.EraDate;
+import charlotte.tools.MathTools;
 import charlotte.tools.TimeData;
 
 public class EraDateTest {
 	public static void main(String[] args) {
 		try {
-			System.out.println("now: " + new EraDate(TimeData.now()));
-			System.out.println("19900108: " + new EraDate(1990, 1, 8));
-			System.out.println("19890108: " + new EraDate(1989, 1, 8));
-			System.out.println("19890107: " + new EraDate(1989, 1, 7));
-			System.out.println("19790906: " + new EraDate(1979, 9, 6));
-			System.out.println("19000101: " + new EraDate(1900, 1, 1));
+			long te = TimeData.now().getT() + 200000000;
+
+			for(long t = 0; t <= te; t += MathTools.random(50000000)) {
+				TimeData td = new TimeData(t);
+
+				System.out.println(td.getString("Y/M/D") + " == " + new EraDate(td).getString());
+			}
 		}
 		catch(Throwable e) {
 			e.printStackTrace();
