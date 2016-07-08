06dd63f03203eec00352d5504e598111a87ac1c6
stackprobe <stackprobe@gmail.com>
2016-07-09 01:06:04 +0900

    b

--- charlotte_test/test_01/Test04.java
+++ charlotte_test/test_01/Test04.java
@@ -13,6 +13,7 @@ public class Test04 {
 	}
 
 	private static void test01() throws Exception {
-		Runtime.getRuntime().exec("CMD /C START \"\" \"C:/Program Files/Common Files\"");
+		Runtime.getRuntime().exec("CMD /C START \"\" \"C:/Program Files/Common Files\"").waitFor(); // open dir
+		//Runtime.getRuntime().exec("CMD /C START \"\" \"C:/var/mp4-layout.txt\"").waitFor(); // open file
 	}
 }
--- charlotte_test/test_01/Test05.java
+++ charlotte_test/test_01/Test05.java
@@ -14,12 +14,22 @@ public class Test05 {
 
 	private static void test01() {
 		try {
-			//throw new RuntimeException();
-			//throw null;
-			throw new OutOfMemoryError();
+			//throw new RuntimeException(); // -> ex
+			//throw null; // -> ex
+			//throw new OutOfMemoryError(); // -> th
+			//doStackOverflow(0); // -> th
+			//byte[] b = new byte[Integer.MAX_VALUE]; // -> th
+			int d = 0; d /= d; // -> ex
 		}
 		catch(Exception e) {
-			System.out.println("ex");
+			System.out.println("ex: " + e);
 		}
+		catch(Throwable e) {
+			System.out.println("th: " + e);
+		}
+	}
+
+	private static void doStackOverflow(int prm) {
+		doStackOverflow(prm + 1);
 	}
 }
