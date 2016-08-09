fe961dbe2bcb164e998edcf3913fadce3375d052
stackprobe <stackprobe@gmail.com>
2016-08-10 01:31:41 +0900

    a

--- charlotte/tools/HugeQueue.java
+++ charlotte/tools/HugeQueue.java
@@ -1,116 +1,63 @@
 package charlotte.tools;
 
 import java.io.Closeable;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class HugeQueue implements Closeable {
-	private String _charset;
-	private String _file;
-	private FileOutputStream _writer;
-	private FileInputStream _reader;
-	private long _size = 0L;
+	private HugeFileQueue _writer;
+	private HugeFileQueue _reader;
 
 	public HugeQueue() {
 		this(StringTools.CHARSET_UTF8);
 	}
 
 	public HugeQueue(String charset) {
-		try {
-			_charset = charset;
-			_file = FileTools.makeTempPath();
-			_writer = new FileOutputStream(_file);
-			_reader = new FileInputStream(_file);
-		}
-		catch(Exception e) {
-			throw new RuntimeException(e);
-		}
+		_writer = new HugeFileQueue(charset);
+		_reader = new HugeFileQueue(charset);
 	}
 
 	public void add(String str) {
-		try {
-			add(str.getBytes(_charset));
-		}
-		catch(Exception e) {
-			throw new RuntimeException(e);
-		}
+		_writer.add(str);
 	}
 
 	public void add(byte[] block) {
-		try {
-			_writer.write(IntTools.toBytes(block.length));
-			_writer.write(block);
-			_size++;
-		}
-		catch(Exception e) {
-			throw new RuntimeException(e);
+		_writer.add(block);
+	}
+
+	private void beforePoll() {
+		if(_reader.size() == 0L && _writer.size() != 0L) {
+			HugeFileQueue swap = _reader;
+			_reader = _writer;
+			_writer = swap;
+			_writer.clear();
 		}
 	}
 
 	public byte[] poll() {
-		if(_size == 0L) {
-			return null;
-		}
-		_size--;
-
-		return readBlock(
-				IntTools.toInt(readBlock(4), 0)
-				);
+		beforePoll();
+		return _reader.poll();
 	}
 
 	public String pollString() {
-		try {
-			byte[] block = poll();
-
-			if(block == null) {
-				return null;
-			}
-			return new String(block, _charset);
-		}
-		catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private byte[] readBlock(int size) {
-		try {
-			byte[] block = new byte[size];
-
-			if(_reader.read(block) != size) {
-				throw new RuntimeException("read error");
-			}
-			return block;
-		}
-		catch(Exception e) {
-			throw new RuntimeException(e);
-		}
+		beforePoll();
+		return _reader.pollString();
 	}
 
 	public long size() {
-		return _size;
+		return _writer.size() + _reader.size();
 	}
 
 	public void clear() {
-		try {
-			FileTools.close(_writer);
-			FileTools.close(_reader);
-			_writer = new FileOutputStream(_file);
-			_reader = new FileInputStream(_file);
-			_size = 0L;
-		}
-		catch(Exception e) {
-			throw new RuntimeException(e);
-		}
+		_writer.clear();
+		_reader.clear();
 	}
 
 	@Override
 	public void close() throws IOException {
-		if(_file != null) {
+		if(_writer != null) {
 			FileTools.close(_writer);
 			FileTools.close(_reader);
-			FileTools.del(_file);
-			_file = null;
+			_writer = null;
 		}
 	}
 }
