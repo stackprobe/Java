3afdb00e1eef7c3c300d4a2984db877183af15dd
stackprobe <stackprobes@gmail.com>
2017-09-07 13:04:54 +0900

    daily

--- bluetears/test09/fresh_green/orange/OrangeAlter.java
+++ bluetears/test09/fresh_green/orange/OrangeAlter.java
@@ -13,39 +13,39 @@ import charlotte.tools.StringTools;
 public class OrangeAlter implements HttSaberAlter {
 	@Override
 	public void flame(HttSaberRequest req) throws HttSaberX {
-		try {
-			System.out.println("OrangeAlter.java flame-req entered");
+		System.out.println("OrangeAlter.java flame-req entered");
 
-			if(req.getUrlString().contains("throwSaberX=OrangeReq")) {
-				HttSaberResponse res = HttArtoria.i().createResponse();
+		if(req.getUrlString().contains("throwSaberX=OrangeReq")) {
+			HttSaberResponse res = HttArtoria.i().createResponse();
 
-				res.getHeaderFields().put("Content-Type", "text/html; charset=UTF-8");
+			res.getHeaderFields().put("Content-Type", "text/html; charset=UTF-8");
+			try {
 				res.setBody("<html><body>throw @ OrangeAlter-req</body></html>".getBytes(StringTools.CHARSET_UTF8));
-
-				throw new HttSaberX(res);
 			}
-		}
-		catch(Throwable e) {
-			throw RunnableEx.re(e);
+			catch(Throwable e) {
+				throw RunnableEx.re(e);
+			}
+
+			throw new HttSaberX(res);
 		}
 	}
 
 	@Override
 	public void flame(HttSaberRequest req, HttSaberResponse res) throws HttSaberX {
-		try {
-			System.out.println("OrangeAlter.java flame-res entered");
+		System.out.println("OrangeAlter.java flame-res entered");
 
-			if(req.getUrlString().contains("throwSaberX=OrangeRes")) {
-				res = HttArtoria.i().createResponse();
+		if(req.getUrlString().contains("throwSaberX=OrangeRes")) {
+			res = HttArtoria.i().createResponse();
 
-				res.getHeaderFields().put("Content-Type", "text/html; charset=UTF-8");
+			res.getHeaderFields().put("Content-Type", "text/html; charset=UTF-8");
+			try {
 				res.setBody("<html><body>throw @ OrangeAlter-res</body></html>".getBytes(StringTools.CHARSET_UTF8));
-
-				throw new HttSaberX(res);
 			}
-		}
-		catch(Throwable e) {
-			throw RunnableEx.re(e);
+			catch(Throwable e) {
+				throw RunnableEx.re(e);
+			}
+
+			throw new HttSaberX(res);
 		}
 	}
 
